299d9fbe5cb1468ef665e21e3793100f36d1971e
Raul Gonzalez <rgonz166@gmail.com>
2019-10-04 21:30:14 -0700

    Current snapshot

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,6 +20,11 @@ module.exports = function(app) {
     });
   });
 
+// Load login page
+  app.get("/login", function(req, res){
+    res.render("login", {});
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
