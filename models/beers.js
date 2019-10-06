95b428f57bfc09bfbb838f623b8e6bae7ded6714
Sky <erniesanchez619@gmial.com>
2019-10-06 15:25:22 -0700

    finish all CRUD functions for beers

--- models/beers.js
+++ models/beers.js
@@ -1,42 +1,45 @@
 //table created for beers_db
 
 module.exports = function(sequelize, DataTypes) {
-  var Beers = sequelize.define("Beers", {
+  var Beers = sequelize.define(
+    "Beers",
+    {
+      category: {
+        //type of beer
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      beername: {
+        //name of the beer
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
 
-    category:{//type of beer
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-
-    },
-    beername: {//name of the beer
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    
-    reviews: {//review of the beer
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        len: [1]
+      reviews: {
+        //review of the beer
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      rating: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
       }
     },
-    rating:{
-      type: DataTypes.DECIMAL, 
-      allowNull:false,
-      validate: {
-        len: [1]
-      }
+    {
+      timestamps: false
     }
-
-      
-  },{
-    timestamps:false
-  });
+  );
   return Beers;
 };
