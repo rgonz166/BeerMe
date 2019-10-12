28a0654997acd546adea8155a0eadbf983d15456
Raul Gonzalez <rgonz166@gmail.com>
2019-10-12 02:27:04 -0700

    Add: posts.sql file with seeds

--- public/js/posts.js
+++ public/js/posts.js
@@ -18,12 +18,32 @@ $(document).ready(function() {
     url: "/api/posts",
     success: function(result) {
       $(postsArea).empty();
+      var beerImg = "";
       // postCounter++;
       // var postsAmount = 4;
       console.log(result);
       // for when we have more posts
       // i < (postsAmount * postCounter)
       for (var i = 0; i < result.length; i++) {
+        switch (result[i].beerName) {
+          case "Stone IPA":
+            beerImg =
+              "https://products0.imgix.drizly.com/ci-stone-ipa-87ab6232002e817e.png?auto=format%2Ccompress&fm=jpeg&q=20";
+            break;
+          case "Pacifico":
+            beerImg =
+              "https://products0.imgix.drizly.com/ci-pacifico-clara-c4083c054131f5c0.png?auto=format%2Ccompress&fm=jpeg&q=20";
+            break;
+          case "Negra Modelo":
+            beerImg ="https://products3.imgix.drizly.com/ci-negra-modelo-5e9fde4dff726bf8.jpeg?auto=format%2Ccompress&fm=jpeg&q=20";
+            break;
+          case "Stone Smoked Porter":
+            beerImg ="https://products1.imgix.drizly.com/ci-stone-smoked-porter-807620fe06a4ed26.jpeg?auto=format%2Ccompress&fm=jpeg&q=20";
+            break;
+          default:
+            beerImg =
+              "https://upload.wikimedia.org/wikipedia/en/6/60/No_Picture.jpg";
+        }
         postsArea.append(
           "<div class='col-lg-3 col-md-4 col-sm-6'>" +
             "<div class='card border-dark'>" +
@@ -34,10 +54,15 @@ $(document).ready(function() {
             "<div class='card-title'>" +
             result[i].beerName +
             "</div>" +
+            "<img src='" +
+            beerImg +
+            "' class='card-img-top' alt='...'>" +
             "<div class='container'>" +
             "<div class='row'>" +
             "<div class='col-mt-3 col-12'>" +
-            "<button class='btn rounded-pill' data-toggle='collapse' data-target='#card-"+(i+1)+"'>" +
+            "<button class='btn rounded-pill' data-toggle='collapse' data-target='#card-" +
+            (i + 1) +
+            "'>" +
             "<div class='center-con'><div class='round'><span></span><span></span><span></span><span></span></div></div></button>" +
             "<div class='collapse' id='card-" +
             (i + 1) +
