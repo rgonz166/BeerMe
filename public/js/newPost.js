c8fc9f8bf9c7e0a0a95f2bcc8a0fc95e171dec08
Raul Gonzalez <rgonz166@gmail.com>
2019-10-11 23:06:28 -0700

    Add: check if logged in before making a post

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -1,61 +1,44 @@
 $(document).ready(function() {
-  // Check Radio-box
-  $(".rating input:radio").attr("checked", false);
+  var userId = Cookies.get("username");
+  if (userId === undefined) {
+    alert("Please login before writing a post.");
+    window.location.href = "/login";
+  } else {
+    // Check Radio-box
+    $(".rating input:radio").attr("checked", false);
 
-  $(".rating input").click(function() {
-    $(".rating span").removeClass("checked");
-    $(this)
-      .parent()
-      .addClass("checked");
-  });
+    $(".rating input").click(function() {
+      $(".rating span").removeClass("checked");
+      $(this)
+        .parent()
+        .addClass("checked");
+    });
 
-  // Getting jQuery references to the post body, title, form, and author select
-  /* Get username from cookie */
-  var userId = $("#user-id");
-  var beerNameInput = $("#beer-name");
-  var reviewInput = $("#review");
-  var ratingSelect;
-  var categorySelect = $("#category");
-  var registerForm = $("#new-post-form");
-  $(".rating input").click(function() {
-    $(".rating span").removeClass("checked");
-    $(this)
-      .parent()
-      .addClass("checked");
-  });
+    // Getting jQuery references to the post body, title, form, and author select
+    /* Get username from cookie */
+    var beerNameInput = $("#beer-name");
+    var reviewInput = $("#review");
+    var ratingSelect;
+    var categorySelect = $("#category");
+    var registerForm = $("#new-post-form");
+    $(".rating input").click(function() {
+      $(".rating span").removeClass("checked");
+      $(this)
+        .parent()
+        .addClass("checked");
+    });
 
-  // Getting jQuery references to the post body, title, form, and author select
-  //   var userId = $("#user-id");
-  var beerNameInput = $("#beer-name");
-  var reviewInput = $("#review");
-  var ratingSelect;
-  var categorySelect = $("#category");
-  var registerForm = $("#new-post-form");
+    // Getting jQuery references to the post body, title, form, and author select
+    //   var userId = $("#user-id");
+    var beerNameInput = $("#beer-name");
+    var reviewInput = $("#review");
+    var ratingSelect;
+    var categorySelect = $("#category");
+    var registerForm = $("#new-post-form");
 
-  $("input:radio").change(function() {
-    ratingSelect = this.value;
-  });
-
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    // Wont submit the post if we are missing a body, title, or author
-    if (
-      !beerNameInput.val().trim() ||
-      !reviewInput.val().trim() ||
-      ratingSelect === undefined ||
-      categorySelect.val() === "select-one"
-    ) {
-      // console.log("Cookie: "+document.cookie);
-      return;
-    }
-    // Constructing a newPost object to hand to the database
-    var newPost = {
-      username: "User1",
-      category: categorySelect.find(":selected").text(),
-      beerName: beerNameInput.val().trim(),
-      review: reviewInput.val().trim(),
-      rating: ratingSelect
-    };
+    $("input:radio").change(function() {
+      ratingSelect = this.value;
+    });
     // Event listener when form is submitted
     $(registerForm).on("submit", handleFormSubmit);
 
